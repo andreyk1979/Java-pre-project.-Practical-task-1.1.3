4722185123ca50226ff8a0eed712a537cd13cf36
andreyk1979 <andrey.kuimov@gmail.com>
2021-10-10 13:48:28 +0200

    Перезалита после переделки в class UserServiceTest измменен    private final UserService userService = new UserServiceImpl();

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl userService = new UserDaoJDBCImpl();
+    private final UserDao userService = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
         userService.createUsersTable();
@@ -17,7 +19,7 @@ public class UserServiceImpl implements UserService {
         userService.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
         userService.saveUser(name,lastName,age);
     }
 
